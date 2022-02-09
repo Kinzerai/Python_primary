98d88c9d84934d90e3a68bb8b4c262167d00bfd6
kinzer <kinzerai@yahoo.com>
2022-02-09 13:03:48 +0300

    Показать последнюю цифру трёхзначного числа.

--- Homework1/Task4-LastDigit.py
+++ Homework1/Task4-LastDigit.py
@@ -1,9 +1,19 @@
 # 4. Показать последнюю цифру трёхзначного числа.
 
-from ast import Num
-import random
+def lastdigit (num):
+    num = num % 100 % 10
+    return num
 
-numA = random.randint(100,1000)
-print(numA)
-numB = numA % 100 % 10
-print(numB)
+number = int(input('Enter 3-digit number: '))
+
+print(f'Third digit of {number} is {lastdigit(number)}')
+
+
+# Без функции:
+# from ast import Num
+# import random
+
+# numA = random.randint(100,1000)
+# print(numA)
+# numB = numA % 100 % 10
+# print(numB)
