cf5b959edc573d9925dbfac824e5a67468bb7e0e
kinzer <kinzerai@yahoo.com>
2022-02-09 13:04:18 +0300

    Показать числа от -N о N

--- Homework1/Task3-NumRange.py
+++ Homework1/Task3-NumRange.py
@@ -1,7 +1,11 @@
 # Показать числа от -N о N
 
-a = int(input('Введите начальное целое отрицательное число:'))
-b = int(input('Введите конечное целое положительное число:'))
+import random
+
+a = random.randint(-10,0)
+b = random.randint(0,10)
+
+print(f'-N = {a}, N = {b}')
 
 for i in range(a, b+1):
     print(i)
